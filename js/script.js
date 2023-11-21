88269404847758bb69ff9d55d65a1ec5228cb1f8
anton <example@gmail.com>
2023-11-21 12:20:01 +0300

    modal window

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ window.addEventListener('DOMContentLoaded', () => {
             }
           });
 
-          //TIME
+          //TIMER
 
   const deadLine = '2023-12-15';
 
@@ -99,4 +99,51 @@ updateClock();
           }
   }
   setClock('.timer', deadLine);
+
+    //MODAL
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalClose = document.querySelector('[data-close]');
+
+     function openModal() {
+            modal.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+            clearInterval(modalTimerId);
+          }
+
+    modalTrigger.forEach(btn => {
+      btn.addEventListener('click', openModal);
+    });
+
+
+      function closeModal() {
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+      }
+
+      modalClose.addEventListener('click', closeModal);
+      
+      modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+          closeModal();
+        }
+      });
+
+      document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape') {
+          closeModal();
+        }
+      });
+
+      const modalTimerId = setTimeout(openModal, 7000);
+
+      function showModalByScroll() {
+        if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+          openModal();
+          window.removeEventListener('scroll', showModalByScroll);
+        }
+      }
+
+      window.addEventListener('scroll', showModalByScroll);
 });
