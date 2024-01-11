d3d6a332a37601da178ab1a1814c2c6e0b076d6a
anton <example@gmail.com>
2024-01-11 11:39:19 +0300

    class

---
+++ js/sam.js
@@ -0,0 +1,11 @@
+const log = function(a, b, ...rest) { 
+console.log(a,b, rest);
+}
+
+log('basic', 'rest', 'perato', 'usage');
+
+function calcOrDouble(number, basis = 2) {
+    console.log(number * basis);
+}
+
+calcOrDouble(3);
