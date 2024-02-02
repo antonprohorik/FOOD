47de3cfbbf87699449d0397ffd673288f8a05d5c
anton <example@gmail.com>
2024-02-02 13:35:23 +0300

    fetch

--- js/script.js
+++ js/script.js
@@ -225,56 +225,73 @@ updateClock();
 
     //FORMS
     const forms = document.querySelectorAll('form');
-
     const message = {
-      loading: 'Загрузка',
-      success: 'Спасибо скоро свяжемся с вами',
-      failer: 'Что-то пошло не так'
+        loading: 'img/form/spinner.svg',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
     };
 
     forms.forEach(item => {
         postData(item);
     });
 
-
     function postData(form) {
-      form.addEventListener('submit', (e) => {
-        e.preventDefault();
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
 
-        const statusMessage = document.createElement('div');
-        statusMessage.classList.add('status');
-        statusMessage.textContent = message.loading;
-        form.append(statusMessage);
+            let statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
+            form.insertAdjacentElement('afterend', statusMessage);
+        
+            const formData = new FormData(form);
 
-        const req = new XMLHttpRequest();
-        req.open('POST', 'server.php');
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
 
-        req.setRequestHeader('Content-Type', 'application/json');
-        const formData = new FormData(form);
-
-        const object = {};
-        formData.forEach(function (value, key) {
-          object[key] = value;
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            }).then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            });
         });
-
-        const json = JSON.stringify(object);
-
-        req.send(json);
-
-        req.addEventListener('load', () => {
-          if (req.status === 200) {
-          console.log(req.response);
-          statusMessage.textContent = message.success;
-          form.reset();
-          setTimeout(() => {
-            statusMessage.remove();
-          }, 2000);
-          } else {
-            statusMessage.textContent = message.failer;
-          }
-        });
-      });
     }
-    
 
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+        openModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+        }, 4000);
+    }
 });
