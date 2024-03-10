67f6e7602a8616e35aa82ec2448b43edfce06bb0
anton <example@gmail.com>
2024-03-10 13:22:35 +0300

    calc

--- js/script.js
+++ js/script.js
@@ -447,9 +447,22 @@ axios.get('http://localhost:3000/menu')
       //CALC
 
   const result = document.querySelector('.calculating__result span');
-  let sex = 'female',
-      height, weight,age,
+  let sex,height, weight,age,ratio;
+
+  if (localStorage.getItem('sex')) {
+    sex = localStorage.getItem('sex');
+  } else {
+    sex = 'female';
+    localStorage.setItem('sex', 'female');
+  }
+
+  if (localStorage.getItem('ratio')) {
+    ratio = localStorage.getItem('ratio');
+    } else {
       ratio = 1.375;
+      localStorage.setItem('ratio', 1.375);
+    }
+
 
   function calcTotal() {
     if (!sex || !height || !weight || !age || !ratio) {
@@ -466,14 +479,34 @@ axios.get('http://localhost:3000/menu')
 
   calcTotal();
 
-  function getStaticInformation(parentSelector, activeClass) {
-    const elements = document.querySelectorAll(`${parentSelector} div`);
+  function initLoclaSettings (selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach(elem => {
+      elem.classList.remove(activeClass);
+      if(elem.getAttribute('id') === localStorage.getItem('sex')) {
+        elem.classList.add(activeClass);
+      }
+      if(elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+        elem.classList.add(activeClass);
+      }
+    });
+  }
+
+  initLoclaSettings('#gender div', 'calculating__choose-item_active');
+  initLoclaSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+
+
+  function getStaticInformation(selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
     elements.forEach(elem => {
       elem.addEventListener('click', (e) => {
         if (e.target.getAttribute('data-ratio')) {
           ratio = +e.target.getAttribute('data-ratio');
+          localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
           } else {
           sex = e.target.getAttribute('id');
+          localStorage.setItem('sex', e.target.getAttribute('id'));
       }
   
       elements.forEach(elem => {
@@ -488,13 +521,20 @@ axios.get('http://localhost:3000/menu')
   }
 
 
-  getStaticInformation('#gender', 'calculating__choose-item_active');
-  getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+  getStaticInformation('#gender div', 'calculating__choose-item_active');
+  getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
 
   function getDinamicInformation(selector) {
     const input = document.querySelector(selector);
 
     input.addEventListener('input', () => {
+
+        if (input.value.match(/\D/g)) {
+          input.style.border = '1px solid red';
+        } else {
+          input.style.border = 'none';
+        }
+
         switch(input.getAttribute('id')) {
           case 'height':
                 height = +input.value;
